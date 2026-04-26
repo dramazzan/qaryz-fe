35449cd01ee8773f2fc5062ae9e24cd226dddf48
dramazzan <ramazandautbek04@gmail.com>
2026-04-27 01:57:34 +0500

    Refactor code structure for improved readability and maintainability

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-a3c37fcbf859f6f9.js","static/chunks/fd9d1056-38967cbf7ee29e57.js","static/chunks/117-b4fd8e37cf83778b.js","static/chunks/main-app-bebcf4063d7fd3bc.js"],pages:{"/_app":["static/chunks/webpack-a3c37fcbf859f6f9.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-d300ddc31afa1fb3.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-a3c37fcbf859f6f9.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-d300ddc31afa1fb3.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
